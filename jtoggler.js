6557ab2030a1eddadc04fd0f95c0dea5e2ba25b5
sinneren <sinneren@gmail.com>
2018-11-02 16:02:11 +0500

    replaced pseudoclass to div

--- jtoggler.js
+++ jtoggler.js
@@ -28,13 +28,15 @@
                 var $control = $('<div />', {
                     class: 'jtoggler-control'
                 });
-                $element.wrap($wrapper).after($control)
+                var $handle = $('<div />', {
+                    class: 'jtoggler-handle'
+                });
+                $control.append($handle);
+                $element.wrap($wrapper).after($control);
             },
 
         } );
 
-        // A really lightweight plugin wrapper around the constructor,
-        // preventing against multiple instantiations
         $.fn[ pluginName ] = function( options ) {
             return this.each( function() {
                 if ( !$.data( this, "plugin_" + pluginName ) ) {
